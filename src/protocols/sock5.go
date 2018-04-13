a9881ca8fe0c29077b7424d986f3e97a7a93f892
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2018-04-13 21:24:01 +1000

    SOCK5 identify stage

--- src/protocols/sock5.go
+++ src/protocols/sock5.go
@@ -8,20 +8,39 @@ import (
 	"io"
 	"encoding/binary"
 	"ident"
+	"errors"
 )
 
 // SOCK5 RFC: http://www.ietf.org/rfc/rfc1928.txt
 
-func sock5Identity(client Client, idents []ident.Identifier) error {
+func sock5Identity(client Client, approved []ident.Identifier) error {
 
-	// read the hand first
+	// read the first message
 	var hand sock5Hand
-	hand.Read(client.Conn)
+	if err := hand.Read(client.Conn); err != nil {
+		return err
+	}
 
-	//
+	// define ident method
+	var identify ident.Identifier
 
+	check:
+	for _, methodID := range hand.Methods {
+		for _, ident := range approved {
 
+			if ident.ID() ==  methodID {
+				identify = ident
+				break check
+			}
+		}
+	}
 
+	// identify client
+	if !identify.Auth() {
+		return errors.New("access denied")
+	}
+
+	return nil
 }
 
 // sock5Hand is the first message from sock5 client
