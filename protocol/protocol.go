1c099fce759b2fa946b7605b517acd7f7570e2b5
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2024-08-05 14:23:19 +0300

    add bind command support

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -90,30 +90,17 @@ func (s Sock5) newCommandState(p *Peer) State {
 		return nil
 	}
 
-	if err := validateProtocolCommand(msg.Cmd); err != nil {
-		p.err = err
-		return s.unsupportedCommand(msg)
-	}
+	switch msg.Cmd {
+	case cmdConnect:
+		return s.connectState(msg)
+	case cmdBind:
+		return s.bindState(msg)
+	case cmdUDPAssoc:
+		return s.commandErrorState(msg, replyStatusNotSupported)
 
-	return s.connectState(msg)
-}
-
-func (s Sock5) unsupportedCommand(msg Command) State {
-	reply := CommandReply{
-		Rep:  replyStatusNotSupported,
-		Rsv:  0,
-		Atyp: msg.Atyp,
-		Addr: msg.Addr,
-		Port: msg.Port,
-	}
-
-	return func(p *Peer) State {
-		if err := p.WriteMessage(reply); err != nil {
-			p.err = fmt.Errorf("sock write: %w", err)
-			return nil
-		}
-
-		return s.newCommandState
+	default:
+		p.err = fmt.Errorf("client sent unsupported commandMessage: %d", msg.Cmd)
+		return s.commandErrorState(msg, replyStatusNotSupported)
 	}
 }
 
@@ -122,7 +109,7 @@ func (s Sock5) connectState(msg Command) State {
 		conn, err := net.Dial("tcp", msg.CanonicalAddr())
 		if err != nil {
 			p.err = fmt.Errorf("dial: %w", err)
-			return s.connectErrorState(msg, replyStatusHostUnreachable)
+			return s.commandErrorState(msg, replyStatusHostUnreachable)
 		}
 
 		reply := CommandReply{
@@ -144,7 +131,7 @@ func (s Sock5) connectState(msg Command) State {
 	}
 }
 
-func (s Sock5) connectErrorState(msg Command, status uint8) State {
+func (s Sock5) commandErrorState(msg Command, status uint8) State {
 	reply := CommandReply{
 		Rep:  status,
 		Rsv:  0,
@@ -163,17 +150,57 @@ func (s Sock5) connectErrorState(msg Command, status uint8) State {
 	}
 }
 
+func (s Sock5) bindState(msg Command) State {
+	return func(p *Peer) State {
+		ls, err := net.Listen("tcp", fmt.Sprintf("%s:0", s.ExternalIP))
+		if err != nil {
+			p.err = fmt.Errorf("bind listen: %w", err)
+			return s.commandErrorState(msg, replyStatusSockFailure)
+		}
+
+		port := uint16(ls.Addr().(*net.TCPAddr).Port)
+		ip := ls.Addr().(*net.TCPAddr).IP
+
+		atyp := atypIpv4
+		if len(ip) != 4 {
+			atyp = atypIpv6
+		}
+
+		reply := CommandReply{
+			Rep:  replyStatusSucceeded,
+			Rsv:  0,
+			Atyp: atyp,
+			Addr: s.ExternalIP,
+			Port: port,
+		}
+
+		// send first reply
+		if err := p.WriteMessage(reply); err != nil {
+			p.err = fmt.Errorf("sock write: %w", err)
+			return nil
+		}
+
+		conn, err := ls.Accept()
+		if err != nil {
+			p.err = fmt.Errorf("bind accept: %w", err)
+			return s.commandErrorState(msg, replyStatusSockFailure)
+		}
+
+		// send first reply (on connect)
+		if err := p.WriteMessage(reply); err != nil {
+			p.err = fmt.Errorf("sock write: %w", err)
+			return nil
+		}
+
+		bind(conn, p.conn)
+
+		return nil
+	}
+}
+
 func validateProtocolVersion(version uint8) error {
 	if version != protoVersion {
 		return fmt.Errorf("client sent unsupported version: %d", version)
-	}
-
-	return nil
-}
-
-func validateProtocolCommand(cmd uint8) error {
-	if cmd != cmdConnect {
-		return fmt.Errorf("client sent unsupported commandMessage: %d", cmd)
 	}
 
 	return nil
