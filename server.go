0508dfb6bc2160bbf0af04b1c9da87daf36d371a
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2024-08-13 09:46:16 +0300

    introduce gssapi contract

--- server.go
+++ server.go
@@ -11,6 +11,63 @@ import (
 	"time"
 )
 
+// GSSAPI provides contract to implement GSSAPI boilerplate.
+// Proxyme refuses client if following methods return non-nil error
+type GSSAPI interface {
+	// AcceptContext accepts client gssapi token produced by gss_init_sec_context.
+	// Token is the opaque authentication token emitted by client GSS-API.
+	// When calling gss_accept_sec_context() for the first time, the
+	// context_handle argument is initially set to GSS_C_NO_CONTEXT.
+	//
+	// For portability, verifier_cred_handle is set to GSS_C_NO_CREDENTIAL
+	// to specify default credentials (for acceptor usage).
+	//
+	// If gss_accept_sec_context returns GssContinueNeeded, the server
+	// should return the generated output_token to the client, and
+	// subsequently pass the resulting client supplied token to another call
+	// to gss_accept_sec_context.
+	//
+	// If gss_accept_sec_context returns GssSComplete, then, if an
+	// output_token is returned, the server should return it to the client.
+	//
+	// If no token is returned, a zero length token should be sent by the
+	// server to signal to the client that it is ready to receive the
+	// client's request.
+	AcceptContext(token []byte) (complete bool, outputToken []byte, err error)
+
+	// AcceptProtectionLevel
+	// The default value of quality of protection shall be specified, and
+	// the use of conf_req_flag shall be as determined by the previous
+	// subnegotiation step.  If protection level 1 is agreed then
+	// conf_req_flag MUST always be FALSE; if protection level 2 is agreed
+	// then conf_req_flag MUST always be TRUE; and if protection level 3 is
+	// agreed then conf_req is determined on a per-message basis by client
+	// and server using local configuration.
+	//
+	// The security context protection level sent by client and server must
+	// be one of the following values:
+	//         1 required per-message integrity
+	//         2 required per-message integrity and confidentiality
+	//         3 selective per-message integrity or confidentiality based on
+	//           local client and server configurations
+	AcceptProtectionLevel(lvl int) (int, error)
+
+	// The token is produced by encapsulating an octet containing the
+	// required protection level using gss_seal()/gss_wrap() with conf_req
+	// set to FALSE.  The token is verified using gss_unseal()/
+	// gss_unwrap().
+	// For TCP and UDP clients and servers, the GSS-API functions for
+	// encapsulation and de-encapsulation shall be used by implementations -
+	// i.e. gss_seal()/gss_wrap(), and gss_unseal()/ gss_unwrap().
+	//
+
+	// Encode produces output token signing/encrypting the data based on protection level.
+	Encode(level int, data []byte) (output []byte, err error)
+
+	// Decode verifies/decrypts token and returns payload.
+	Decode(token []byte) (data []byte, err error)
+}
+
 type Options struct {
 	// BindIP is public server interface (IP v4/v6) for protocol BIND operation (incoming traffic
 	// from outside to client sock).
@@ -23,7 +80,9 @@ type Options struct {
 	// Authenticate enables USERNAME/PASSWORD authentication method.
 	// Checks user credentials, non nil error causes DENIED status for client.
 	Authenticate func(username, password []byte) error
+
 	// TODO: GSSAPI
+	GSSAPI func() GSSAPI
 
 	// Connect establishes tcp sock connection to remote server, addr is host:port string.
 	// If not specified, default dialer will be used that just net.Dial to remote server.
