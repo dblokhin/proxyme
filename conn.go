c3b1cc0906117f16d3e95243a940c71f7f3cdf16
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2024-08-24 20:24:24 +0300

    refoctor: move gssconn to apropriate place

--- conn.go
+++ conn.go
@@ -1,11 +1,13 @@
 package proxyme
 
 import (
+	"bytes"
 	"io"
 	"net"
 	"time"
 )
 
+// tcpConnWithTimeout enables idle timeout on connection
 type tcpConnWithTimeout struct {
 	*net.TCPConn
 	timeout time.Duration
@@ -29,4 +31,59 @@ func (t tcpConnWithTimeout) Write(p []byte) (n int, err error) {
 func (t tcpConnWithTimeout) Read(p []byte) (n int, err error) {
 	_ = t.TCPConn.SetDeadline(time.Now().Add(t.timeout)) // nolint
 	return t.TCPConn.Read(p)
+}
+
+// gssConn is encapsulated GSSAPI connection.
+type gssConn struct {
+	raw    io.ReadWriteCloser
+	gssapi GSSAPI
+	buffer bytes.Buffer
+}
+
+func (g gssConn) Read(p []byte) (int, error) {
+	// from raw conn -> gssapi decode -> encapsulated conn
+	var msg gssapiMessage
+
+	if g.buffer.Len() > 0 {
+		return g.buffer.Read(p)
+	}
+
+	_, err := msg.ReadFrom(g.raw)
+	if err != nil {
+		return 0, err
+	}
+
+	if err := msg.validate(gssEncapsulation); err != nil {
+		return 0, err
+	}
+
+	payload, err := g.gssapi.Decode(msg.token)
+	if err != nil {
+		return 0, err
+	}
+
+	n := min(len(p), len(payload))
+	copy(p, payload)
+
+	if n < len(payload) {
+		if _, err := g.buffer.Write(payload[n:]); err != nil {
+			return n, err
+		}
+	}
+
+	return n, nil
+}
+
+func (g gssConn) Write(p []byte) (n int, err error) {
+	// from encapsulated conn -> gssapi encode -> raw conn
+	token, err := g.gssapi.Encode(p)
+	if err != nil {
+		return 0, err
+	}
+
+	return g.raw.Write(token)
+}
+
+func (g gssConn) Close() error {
+	return g.raw.Close()
 }
